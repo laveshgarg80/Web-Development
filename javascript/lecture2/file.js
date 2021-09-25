6f0278a26abffa3771bb265ec06296b579bd00f9
laveshgarg80 <loveshgarg80@gmail.com>
2021-09-25 14:44:48 +0530

    lecture2

--- javascript/lecture2/file.js
+++ javascript/lecture2/file.js
@@ -1,5 +1,5 @@
 //you do not have to install fs package
 //fs package is already in the node for our usage
 let fs = require("fs");
-let readedText = fs.readFileSync("../lecture1/f3.txt", "utf-8");
+let readedText = fs.readFileSync("./f1.txt", "utf-8");
 fs.writeFileSync("./f2.txt", readedText, "utf-8");
